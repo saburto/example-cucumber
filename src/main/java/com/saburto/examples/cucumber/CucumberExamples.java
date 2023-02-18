3eeb803b28093503daf0e8a96bc9e409c75316b0
Sebastian Aburto <seba.aburto@gmail.com>
2023-02-18 09:37:11 +0100

    first commit

---
+++ src/main/java/com/saburto/examples/cucumber/CucumberExamples.java
@@ -0,0 +1,13 @@
+package com.saburto.examples.cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberExamples {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberExamples.class, args);
+	}
+
+}
